06d29e60a147f791381bfe3cd4e9739cbe177720
ACER\Z1402 <octaviaalya01@gmail.com>
2021-12-30 07:48:57 +0700

    add migration for shopping chart tables

---
+++ database/migrations/2021_11_20_154952_create_shopping_carts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShoppingCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buyer_id')->constrained('users');
+            $table->foreignId('goods_id')->constrained('goods');
+            $table->foreignId('seller_id')->constrained('users');
+            $table->integer('qty')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_carts');
+    }
+}
